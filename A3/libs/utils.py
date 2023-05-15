32f98179e2231611976a264f9ef869b22cc217f9
rderoncio <rafael.deroncio@hotmail.com>
2023-05-15 19:40:59 -0300

    add validacao de atraso

--- A3/libs/utils.py
+++ A3/libs/utils.py
@@ -11,6 +11,7 @@ class Utils(Enum):
     num_voo = {'tipo': str, 'descricao': 'Número do voo'}
     companhia_aerea = {'tipo': str, 'descricao': 'Nome da companhia aérea'}
     codigo_tipo_linha = {'tipo': str, 'descricao': 'Código do tipo de linha (internacional)'}
+
     aeroporto_origem = {'tipo': str, 'descricao': 'Código do aeroporto de origem'}
     cidade_origem = {'tipo': str, 'descricao': 'Cidade de origem'}
     estado_origem = {'tipo': str, 'descricao': 'Estado de origem'}
@@ -19,6 +20,8 @@ class Utils(Enum):
     long_orig = {'tipo': float, 'descricao': 'Longitude do aeroporto de origem'}
     partida_prevista = {'tipo': datetime, 'descricao': 'Horário previsto de partida'}
     partida_real = {'tipo': datetime, 'descricao': 'Horário real de partida'}
+    partida_atrasou = {'tipo': bool, 'descricao': 'Houve atraso na partida do voo?'}
+
     aeroporto_destino = {'tipo': str, 'descricao': 'Código do aeroporto de destino'}
     cidade_destino = {'tipo': str, 'descricao': 'Cidade de destino'}
     estado_destino = {'tipo': str, 'descricao': 'Estado de destino'}
@@ -27,7 +30,10 @@ class Utils(Enum):
     long_dest = {'tipo': float, 'descricao': 'Longitude do aeroporto de destino'}
     chegada_prevista = {'tipo': datetime, 'descricao': 'Horário previsto de chegada'}
     chegada_real = {'tipo': datetime, 'descricao': 'Horário real de chegada'}
+    chegada_atrasou = {'tipo': bool, 'descricao': 'Houve atraso na chegada do voo?'}
+
     distancia_km = {'tipo': float, 'descricao': 'Total em quilômetros entre o local de origem e destino'}
+    
     situacao_voo = {'tipo': str, 'descricao': 'Situação do voo (realizado, cancelado, etc)'}
     codigo_justificativa = {'tipo': str, 'descricao': 'Código de justificativa (caso o voo tenha sido cancelado)'}
 
@@ -68,7 +74,7 @@ class Utils(Enum):
         return Utils.__dict__[nome_coluna].__dict__['_value_']['descricao']
 
     @staticmethod
-    def calcular_distancia_km(lat_orig:float, long_orig:float, lat_dest:float, long_dest:float) -> float:
+    def calcular_distancia_km(lat_orig: float, long_orig: float, lat_dest: float, long_dest: float) -> float:
         """
         Calcula a distância em km entre dois pontos (latitude, longitude) utilizando a fórmula Haversine.
 
@@ -121,3 +127,31 @@ class Utils(Enum):
         dataframe[nome_coluna] = pd.to_datetime(dataframe[nome_coluna])
         dataframe[nome_coluna] = dataframe[nome_coluna].dt.strftime('%d/%m/%Y %H:%M:%S')
         return dataframe
+
+    @staticmethod
+    def validar_atraso(situacao: str, dt_prevista: str, dt_real: str) -> str:
+        """
+        Verifica se um voo está atrasado comparando os horários previsto e real de partida/chegada.
+        Se a situação do voo for 'cancelado', retorna uma string vazia.
+        Se os horários previsto e real forem iguais ou se o horário previsto for maior ou igual ao horário real,
+        retorna 'N', caso contrário, retorna 'S'.
+
+        Args:
+            situacao (str): Situação do voo ('cancelado' ou 'realizado').
+            prevista (str): Horário previsto de partida/chegada no formato 'dd/mm/aaaa hh:mm:ss'.
+            real (str): Horário real de partida/chegada no formato 'dd/mm/aaaa hh:mm:ss'.
+
+        Returns:
+            str: 'N' caso o voo esteja atrasado ou 'S' caso contrário. Retorna uma string vazia se o voo foi cancelado.
+        """
+
+        if situacao.lower() == 'cancelado':
+            return ''
+        
+        prevista_datetime = datetime.strptime(dt_prevista, '%d/%m/%Y %H:%M:%S')
+        real_datetime = datetime.strptime(dt_real, '%d/%m/%Y %H:%M:%S')
+
+        if prevista_datetime >= real_datetime or prevista_datetime == real_datetime:
+            return 'N'
+        else:
+            return 'S'
